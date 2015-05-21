c7b76fae6f85ff0844eabf8c9922c672537581a0
David DeBoer <ddeboer@berkeley.edu>
2015-05-21 09:32:41 -0700

    fixed some latex errors and updated bounces

--- bounceatten.py
+++ bounceatten.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 A = np.arange(5,21,0.1)               # atten/bounce
-b = [1.0,3.0,5.0]             # bounces in 1,2,3
+b = [2.0,4.0,5.0]             # bounces in 1,2,3
 
 
 plt.figure(1)
@@ -14,10 +14,10 @@ xl = [A[0],A[-1]]
 yl = [60.0,60.0]
 plt.plot(xl,yl,'--')
 
-plt.xlabel('Attenuation/bounce [dB]')
+plt.xlabel('Attenuation/reflection [dB]')
 plt.ylabel('Attenuation [dB]')
 plt.axis([5,21,0,80])
 plt.grid()
-plt.text(18.2,15,'1',fontsize=14)
-plt.text(15.0,41,'2',fontsize=14)
-plt.text(9,42,'3',fontsize=14)
+plt.text(18.2,32,'1',fontsize=14)
+plt.text(13.0,49,'2',fontsize=14)
+plt.text(9,47,'3',fontsize=14)
